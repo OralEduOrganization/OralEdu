3a832d76d22468a6f15c26b06eb2b54895107f41
asiuvxmcn <502419517@qq.com>
2016-07-04 18:37:04 +0800

    修改

---
+++ OralEdu/pickImageView.h
@@ -0,0 +1,13 @@
+//
+//  pickImageView.h
+//  OralEdu
+//
+//  Created by 刘芮东 on 16/7/4.
+//  Copyright © 2016年 wangjungang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface pickImageView : UIView
+
+@end
---
+++ OralEdu/pickImageView.m
@@ -0,0 +1,22 @@
+//
+//  pickImageView.m
+//  OralEdu
+//
+//  Created by 刘芮东 on 16/7/4.
+//  Copyright © 2016年 wangjungang. All rights reserved.
+//
+
+#import "pickImageView.h"
+
+@implementation pickImageView
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        
+    }
+    return self;
+}
+
+@end
